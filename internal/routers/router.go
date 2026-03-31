818222676aebe89813496f3e0ac3822b228453fc
woshashishi <2578160247@qq.com>
2026-03-31 19:16:12 +0800

    init: blog-service 项目初始化

---
+++ internal/routers/router.go
@@ -0,0 +1,41 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+	_ "github.com/woshashishishi/blog-service/docs"
+	"github.com/woshashishishi/blog-service/internal/middleware"
+	"github.com/woshashishishi/blog-service/internal/routers/api"
+	v1 "github.com/woshashishishi/blog-service/internal/routers/api/v1"
+)
+
+func NewRouter() *gin.Engine {
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	r.Use(middleware.Translations())
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	article := v1.NewArticle()
+	tag := v1.NewTag()
+	upload := api.NewUpload()
+	r.POST("/upload/file", upload.UploadFile)
+	r.POST("/auth", api.GetAuth)
+	apiv1 := r.Group("/api/v1")
+	apiv1.Use(middleware.JWT())
+	{
+		apiv1.POST("/tags", tag.Create)
+		apiv1.DELETE("/tags/:id", tag.Delete)
+		apiv1.PUT("/tags/:id", tag.Update)
+		apiv1.PATCH("/tags/:id/state", tag.Update)
+		apiv1.GET("/tags", tag.List)
+
+		apiv1.POST("/articles", article.Create)
+		apiv1.DELETE("/articles/:id", article.Delete)
+		apiv1.PUT("/articles/:id", article.Update)
+		apiv1.PATCH("/articles/:id/state", article.Update)
+		apiv1.GET("/articles/:id", article.Get)
+		apiv1.GET("/articles", article.List)
+	}
+	return r
+}
---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/woshashishishi/blog-service/global"
+	"github.com/woshashishishi/blog-service/internal/model"
+	"github.com/woshashishishi/blog-service/internal/routers"
+	"github.com/woshashishishi/blog-service/pkg/logger"
+	"github.com/woshashishishi/blog-service/pkg/setting"
+)
+
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+	err = setupLogger()
+	if err != nil {
+		log.Fatalf("init.setupLogger err: %v", err)
+	}
+}
+
+// @title 博客系统
+// @version 1.0
+// @description Go 语言编程之旅：一起用 Go 做项目
+// @termsOfService https://github.com/go-programming-tour-book
+func main() {
+	err := setupDBEngine(global.DatabaseSetting)
+	if err != nil {
+		log.Fatalf("init.setupDBEngine err: %v", err)
+	}
+	gin.SetMode(global.ServerSetting.RunMode)
+
+	r := routers.NewRouter()
+	s := &http.Server{
+		Addr:           ":" + global.ServerSetting.HttpPort,
+		Handler:        r,
+		ReadTimeout:    global.ServerSetting.ReadTimeout,
+		WriteTimeout:   global.ServerSetting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+	fmt.println("adadadsadas")
+	global.Logger.Infof("%s: go-programming-tour-book/%s", "eddycjy", "blog-service")
+	s.ListenAndServe()
+
+}
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("JWT", &global.JWTSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Email", &global.EmailSetting)
+	if err != nil {
+		return err
+	}
+
+	global.JWTSetting.Expire *= time.Second
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
+}
+func setupDBEngine(databaseSetting *setting.DatabaseSettingS) error {
+	var err error
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func setupLogger() error {
+	global.Logger = logger.NewLogger(&lumberjack.Logger{
+		Filename:  global.AppSetting.LogSavePath + "/" + global.AppSetting.LogFileName + global.AppSetting.LogFileExt,
+		MaxSize:   600,
+		MaxAge:    10,
+		LocalTime: true,
+	}, "", log.LstdFlags).WithCaller(2)
+
+	return nil
+}
